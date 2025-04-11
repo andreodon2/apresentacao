f817c163285a10a97f51c5184fee471cf2154da5
André Gabriel Odon de Freitas <andreodon@gmail.com>
2025-04-11 17:21:33 -0300

    Apresentação em React

---
+++ Vite/apresentacao/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return(
+        <>
+        <header classname="cabecalho">
+            <h1>André Gabriel Odon de Freitas</h1>
+            <p><strong>Brasileiro, casado, 35 anos</strong><br/>
+            <strong>Belém/PA</strong></p>
+        </header>
+    </>
+    )
+    
+}
+
+export default Header;
