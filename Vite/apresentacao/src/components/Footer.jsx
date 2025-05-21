7ee3a9c687cc084193ef3cbb1be537d3e95a66af
André Gabriel Odon de Freitas <andreodon@gmail.com>
2025-05-21 17:35:21 -0300

    Conclussão do footer

--- Vite/apresentacao/src/components/Footer.jsx
+++ Vite/apresentacao/src/components/Footer.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import '../App.css';
+import LogoInsta from '../assets/logo-insta.png'
+import LogoLinkedin from '../assets/logo-linkedin.png'
+import LogoGmail from '../assets/logo-gmail.png'
+
+function enviarFormulario(){
+                        let nome = window.document.querySelector('input#nome').value;
+                        let email = window.document.querySelector('input#email').value;
+                        let msg = window.document.querySelector('textarea#msg').value;
+
+                        let dadosFormulario = {
+                            nome: nome,
+                            email: email,
+                            msg: msg
+                        };
+                    }
 
 function Footer(){
     return (
         <>
             <h3>Acesse minhas redes sociais</h3>
             <div class="redes-sociais">    
-                <a href="https://www.instagram.com/andreodonn/" rel="noopener noreferrer"><img src="img/logo-insta.png" alt="Logo do instagram em efeito negativo" width="3%"/></a>
+                <a href="https://www.instagram.com/andreodonn/" rel="noopener noreferrer"><img src={LogoInsta} alt="Logo do instagram em efeito negativo" width="3%"/></a>
 
-                <a href="https://www.linkedin.com/in/andreodon/" rel="noopener noreferrer"><img src="img/logo-linkedin.png" alt="Logo do instagram em efeito negativo" width="3%"/></a>
+                <a href="https://www.linkedin.com/in/andreodon/" rel="noopener noreferrer"><img src={LogoLinkedin} alt="Logo do instagram em efeito negativo" width="3%"/></a>
 
-                <a href="mailto:andreodon@gmail.com?subject=Vim de seu site" target="_blank"><img src="img/logo-gmail.png" alt="Logo do instagram em efeito negativo" width="3%"/></a>
+                <a href="mailto:andreodon@gmail.com?subject=Vim de seu site" target="_blank"><img src={LogoGmail} alt="Logo do instagram em efeito negativo" width="3%"/></a>
             </div>
 
             <h4>Entre em contato comigo</h4>
+                <form>
+                    <strong>Nome: </strong><input type="text" name="nome" id="nome" placeholder="Escreva seu nome"/><br/>
+
+                    <strong>Email: </strong><input type="email" name="email" id="email" placeholder="seuemail@email.com.br"/>
+
+                    <h4>Esvreva sua mensagem</h4>
+                    <textarea rows="8" cols="40" id="msg">
+                        
+                    </textarea><br/>
+                    <input class="botao" type="onSubmit" onclick="enviarFormulario()"/>
+                </form>
                 [FAZER O FORMULÁRIO]
 
                 <p>
